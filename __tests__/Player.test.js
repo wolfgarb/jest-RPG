7b177cccd26d5d8dcc3e4f610b966fb9396f2c70
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-11-02 11:54:33 -0400

    10.2.7

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,3 +15,21 @@ test('creates a player object', () => {
     );
   });
 
+test('gets players stats as an obj', () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
